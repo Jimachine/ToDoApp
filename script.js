424162a88db4455bdad4eb8d5322c4931dc93cee
Jimachine <jimmy_onion@hotmail.com>
2020-01-17 09:38:47 +0000

    Now correctly adding tasks to a list

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.querySelector("user-input").addEventListener("submit", addTask);
+document.querySelector("#user-input").addEventListener("submit", addTask);
 
 function addTask(e)
 
         {
             e.preventDefault();
-            let input = document.querySelector("task");
-            console.log(input);
 
-            if (input.value !== "") {
-                document.querySelector("para").innerHTML = input;
+            if (input.value !== ""){
+                
+                input = document.querySelector("#task");
+                var node = document.createElement("LI");
+                var textnode = document.createTextNode(input.value);
+                node.appendChild(textnode);
+                document.getElementById("task-list").appendChild(node);
+                document.getElementById("task").value = "";
             }
         }
