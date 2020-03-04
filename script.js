913f55440e0a6217abe2df4d64e3faed4385328f
Jimachine <jimmy_onion@hotmail.com>
2020-03-04 10:41:23 +0000

    Added clear all button, need to add functionality for re ordering

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.querySelector("#user-input").addEventListener("submit", addTask);
+document.querySelector("#user-input").addEventListener("reset", clearAll);
 document.querySelector("#task-list").addEventListener("click", clicked);
 
 function addTask(e)
@@ -11,17 +12,33 @@ function addTask(e)
             {
                 let ul = document.querySelector('ul');
                 let li = document.createElement('li');
-                li.innerHTML = '<input type="checkbox" class="checkbox">' + '<p class="unchecked">' + input.value + '</p>' + '<i class="fas fa-trash"></i>';
+                li.innerHTML = '<input type="checkbox" class="checkbox">'
+                                + '<p class="unchecked">' + input.value + '</p>'
+                                + '<i class="fas fa-arrow-up"></i>'
+                                + '<i class="fas fa-arrow-down"></i>'
+                                + '<i class="fas fa-trash"></i>'
                 ul.appendChild(li);
                 document.getElementById("task").value = "";
 
             }
         }
 
+function clearAll()
+
+        {
+            let tasks = document.querySelector("#task-list");
+
+            while (tasks.hasChildNodes()) {
+                tasks.removeChild(tasks.firstChild);
+            }
+
+        }
+
 function clicked(e)
 
         {  
-                if (e.target.tagName == "I")
+
+                if (e.target.className == "fas fa-trash")
                 {
                     var target = e.target;
                     var toDelete = target.parentNode;
@@ -29,7 +46,7 @@ function clicked(e)
                     parentNode.removeChild(toDelete);
                 }
 
-                else if (e.target.tagName == "INPUT")
+                else if (e.target.className == "checkbox")
                 {
                     if (e.target.checked == true) {
                         e.target.parentNode.querySelector(".unchecked").className = "checked";
@@ -38,5 +55,10 @@ function clicked(e)
                     else {
                         e.target.parentNode.querySelector(".checked").className = "unchecked";
                     }
+                }
+
+                else if (e.target.className == "fas fa-arrow-down")
+                {
+                    
                 }
         }
