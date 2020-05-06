072a2678e8ecba7a0b08eed79ee2f56449f43036
Jimachine <jimmy_onion@hotmail.com>
2020-05-06 11:41:05 +0100

    re-ordering now works correctly + refactoring

--- script.js
+++ script.js
@@ -4,35 +4,44 @@ document.querySelector("#task-list").addEventListener("click", clicked);
 
 let tasksArray = [];
 
-function addTask(e)
+function addTask(e) {
+    e.preventDefault();
+    // Store text entered into the input field
+    let input = document.querySelector("#task");
 
-        {
-            e.preventDefault();
-            // Store text entered into the input field
-            let input = document.querySelector("#task");
+    if (input.value !== "")
+    {
 
-            if (input.value !== "")
-            {
-                let ul = document.querySelector('ul');
-                let li = document.createElement('li');
-                let inputField = document.getElementById("task");
+        tasksArray.push({ name: input.value, complete: false });
+        let ul = document.querySelector('ul');
+        
+        renderTasks();
+        
+        // Clear the input field ready for the next task
+        input.value = "";
 
-                li.innerHTML = '<input type="checkbox" class="checkbox">'
-                                + '<p class="unchecked">' + input.value + '</p>'
-                                + '<i class="fas fa-arrow-up"></i>'
-                                + '<i class="fas fa-arrow-down"></i>'
-                                + '<i class="fas fa-trash"></i>'
+    }
+}
 
-                // Add the list item to the list
-                ul.appendChild(li);
-                // Add the list item to an array
-                tasksArray.push(li);
-                
-                // Clear the input field ready for the next task
-                inputField.value = "";
+function renderTasks () {
 
-            }
-        }
+    let ul = document.querySelector('ul');
+    ul.innerHTML = '';
+
+    tasksArray.forEach((task, index) => {
+        let li = document.createElement('li');
+        li.setAttribute('data-order', index);
+
+        li.innerHTML = `<input type="checkbox" class="checkbox" ${task.complete ? "checked" : ""}>
+                        <p>${task.name}</p>
+                        <i class="fas fa-arrow-up"></i>
+                        <i class="fas fa-arrow-down"></i>
+                        <i class="fas fa-trash"></i>`
+
+        // Add the list item to the list
+        ul.appendChild(li);
+    });    
+}
 
 function clearAll()
 
@@ -53,43 +62,34 @@ function clicked(e)
                 // Delete an individual list item when the trash can is clicked
                 if (e.target.className == "fas fa-trash")
                 {
-                    var target = e.target;
-                    var toDelete = target.parentNode;
-                    var parentNode = toDelete.parentNode;
+                    tasksArray.forEach((task, index) => {
+                        let target = e.target.parentNode.getAttribute("data-order");
+                        console.log(index);
+                        console.log(target);
 
-                    // Find the matching list item in the array by looping through each index and then deleting it
-                    for ( i = 0; i < tasksArray.length; i++){
-                        if(tasksArray[i] == toDelete){
-                            delete tasksArray[i];
+                        if(index == target)
+                        {
+                            tasksArray.splice(index, 1);
+                            renderTasks();
                         }
-                    }
-                    parentNode.removeChild(toDelete);
-                }
-
-                // Change the class name of the checkbox to indicate whether the text should be scored out or not
-                else if (e.target.className == "checkbox")
-                {
-                    if (e.target.checked == true) {
-                        e.target.parentNode.querySelector(".unchecked").className = "checked";
-                    }
-
-                    else {
-                        e.target.parentNode.querySelector(".checked").className = "unchecked";
-                    }
+                    })
                 }
 
                 // Move a list item down one space
                 else if (e.target.className == "fas fa-arrow-down")
                 {
                     let ul = document.querySelector('ul');
-                    let li = document.createElement('li');
+                    let items = document.querySelectorAll('li');
                     
                     for (i = 0; i < tasksArray.length; i++){
                         // Find a match in the array with the list item clicked and make sure it isnt the last index
+                        console.log(tasksArray[i] == e.target.parentNode);
+                        console.log(e.target.parentNode != tasksArray[tasksArray.length - 1]);
                         if(tasksArray[i] == e.target.parentNode && e.target.parentNode != tasksArray[tasksArray.length - 1]){
                             
                             // List item below the one which was clicked
                             let moveUpPage = tasksArray[i + 1];
+                            
 
                             // Clicked list item
                             let moveDownPage = tasksArray[i];
@@ -97,6 +97,8 @@ function clicked(e)
                             // Re add the items to the array in the corrected positions
                             tasksArray.splice(i, 1, moveUpPage);
                             tasksArray.splice(i + 1, 1, moveDownPage);
+
+                            break;
                         }
 
                     }
@@ -104,7 +106,9 @@ function clicked(e)
                     ul.innerHTML = '';
 
                         for (i = 0; i < tasksArray.length; i++){
-                            
+
+                            let li = document.createElement('li');
+
                             li.innerHTML = tasksArray[i].innerHTML;
                             ul.appendChild(li);
                     }
