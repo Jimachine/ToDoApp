8768900783c25dbf043e53e83715568487f28674
Jimachine <jimmy_onion@hotmail.com>
2020-06-17 19:32:55 +0100

    Tasks can be re-ordered using drag and drop

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 document.querySelector("#user-input").addEventListener("submit", addTask);
 document.querySelector("#user-input").addEventListener("reset", clearAll);
 document.querySelector("#task-list").addEventListener("click", clicked);
-
+document.addEventListener("dragover", function(event) {event.preventDefault();});
+document.addEventListener("dragstart", handleDragStart);
+document.addEventListener("dragend", handleDragEnd);
+document.addEventListener("drop", handleDropEvent);
 
 var lsTasks = localStorage.getItem('tasks');
 var tasksArray = [];
+var dragged;
 
-if (lsTasks != ""){
+if (lsTasks != "") {
     tasksArray = JSON.parse(lsTasks);
     renderTasks()
 }
-
-
 
 function addTask(e) {
     e.preventDefault();
@@ -29,7 +31,7 @@ function addTask(e) {
         updateLS();
     }
 }
-
+//Save the current tasks to local storage
 function updateLS() {
     var stringTasks = JSON.stringify(tasksArray);
     localStorage.setItem('tasks', stringTasks);
@@ -43,6 +45,8 @@ function renderTasks () {
     tasksArray.forEach((task, index) => {
         let li = document.createElement('li');
         li.setAttribute('data-order', index);
+        li.setAttribute('draggable', true);
+        li.className = 'list-item';
 
         li.innerHTML = `<input type="checkbox" class="checkbox" ${task.complete ? "checked" : ""}>
                         <p>${task.name}</p>
@@ -56,9 +60,39 @@ function renderTasks () {
     updateLS();
 }
 
-function clearAll()
 
-        {
+
+
+function handleDragStart(e) {
+    dragged = e.target;
+    dragged.style.border = "thin dotted red";
+}
+
+function handleDragEnd(e) {
+    dragged.style.border = "none";
+}
+
+function handleDropEvent(e) {
+
+        // Grab the data-order attribute from the element that was dropped to
+        let dropTarget = e.target.getAttribute("data-order");
+        // Take the object data out of the array from the element which was dragged
+        let dragTarget = tasksArray[dragged.getAttribute("data-order")];
+
+        // Delete the dragged element's object data from the array
+        tasksArray.splice(dragged.getAttribute("data-order"), 1);
+        // Re-insert the dragged element's data into the array at the new index
+        tasksArray.splice(dropTarget, 0, dragTarget);
+        
+        renderTasks();
+        updateLS();
+}
+
+
+
+
+
+function clearAll() {
             //Clear all tasks currently in the list
             let tasks = document.querySelector("#task-list");
 
@@ -70,9 +104,7 @@ function clearAll()
             updateLS();
         }
 
-function clicked(e)
-
-        {  
+function clicked(e) {  
                 // Delete an individual list item when the trash can is clicked
                 if (e.target.className === "fas fa-trash")
                 {
